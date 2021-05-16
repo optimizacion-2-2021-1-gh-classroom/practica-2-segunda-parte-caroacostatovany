09af44ebc96f014823a9dfd2e21d590ade798e35
caroacostatovany <carolina.acostatovany@gmail.com>
2021-05-15 23:45:58 -0500

    Arreglar bug de Nonetype en pivoteos

--- src/mex/simplex/simplex_networks.py
+++ src/mex/simplex/simplex_networks.py
@@ -125,25 +125,25 @@ def find_pivot_col(matrix):
         
         c (int): index smallest value on row.
     """
-    
+
     total = []
     neg = find_negative_col(matrix)
     row = matrix[neg, :-1]
     m = min(row)
     c = np.where(row == m)[0][0]
     col = matrix[:-1, c]
-    for i,j in zip(col, matrix[:-1,-1]):  #i for col with neg, j for right col
+    for i, j in zip(col, matrix[:-1, -1]):  # i for col with neg, j for right col
         if i != 0:
-            tmp = j/i
+            tmp = j / i
             if tmp > 0:
                 total.append(tmp)
             else:
-                total.append(10000)
+                total.append(np.nan)
         else:
-            total.append(10000) #placeholder, might need to update for large scale
-    index = total.index(min(total))
-    
-    return [index,c]
+            total.append(np.nan)  # placeholder, might need to update for large scale
+    index = total.index(min(i for i in total if not np.isnan(i)))
+
+    return [index, c]
 
 
 def find_pivot_row(matrix):
@@ -160,8 +160,7 @@ def find_pivot_row(matrix):
         
         neg (int): index smallest value.
     """
-    
-    #if pivots_row(matrix):
+
     total = []
     neg = find_negative_row(matrix)
     for i, j in zip(matrix[:-1, neg], matrix[:-1, -1]):
@@ -170,15 +169,15 @@ def find_pivot_row(matrix):
             if tmp > 0:
                 total.append(tmp)
             else:
-                total.append(10000)
+                total.append(np.nan)
         else:
-            total.append(10000) #placeholder, might need to update for large scale
-    index = total.index(min(total))
+            total.append(np.nan) #placeholder, might need to update for large scale
+    index = total.index(min(i for i in total if not np.isnan(i)))
 
     return [index,neg]
 
 
-def pivot(row,col,matrix):
+def pivot(row, col, matrix):
     """
     Pivot about a value to remove negative in final column or row.
     
