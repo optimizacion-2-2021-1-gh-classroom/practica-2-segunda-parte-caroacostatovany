31cadbf4644cf0f016a5c9ec47348891282b310c
caroacostatovany <carolina.acostatovany@gmail.com>
2021-05-18 21:28:54 -0500

    Agregar generate tableu para restricciones de la matriz

--- src/mex/simplex/maximizer_class.py
+++ src/mex/simplex/maximizer_class.py
@@ -1,15 +1,21 @@
 import numpy as np
 from mex.simplex.simplex_networks import create_matrix
 from mex.simplex.problem_definition import constrain, obj, maxz
-from mex.utils.general import generates_matrix
+from mex.utils.general import generates_matrix, generate_tableau
 
 
 class Maximizer():
     """Maximize the objective function"""
     def __init__(self, A, b, c):
+        self.A_matrix = A
+        self.b_vector = b
+        self.c_vector = c
         self.matrix = generates_matrix(A, b, c)
         self.max = None
         self.coeff = None
+
+    def add_constraints(self, lowerbounds, upperbounds):
+        self.matrix = generate_tableau(self.A_matrix, self.b_vector, lowerbounds, upperbounds, self.c_vector, compr=True)
 
     def solve(self):
         solve = maxz(self.matrix)
--- src/mex/simplex/minimizer_class.py
+++ src/mex/simplex/minimizer_class.py
@@ -1,15 +1,21 @@
 import numpy as np
 from mex.simplex.simplex_networks import create_matrix
 from mex.simplex.problem_definition import constrain, obj, minz
-from mex.utils.general import generates_matrix
+from mex.utils.general import generates_matrix, generate_tableau
 
 
 class Minimizer():
     """Minimize the objective function"""
     def __init__(self, A, b, c):
+        self.A = A
+        self.b = b
+        self.c = c
         self.matrix = generates_matrix(A, b, [-1*x for x in c])
         self.min = None
         self.coeff = None
+
+    def add_constraints(self, lowerbounds, upperbounds):
+        self.matrix = generate_tableau(self.A_matrix, self.b_vector, lowerbounds, upperbounds, self.c_vector, compr=True)
 
     def solve(self):
         solve = minz(self.matrix)
